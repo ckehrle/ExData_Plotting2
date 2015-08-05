d1a4e64f78aefa57f9bff288959885b05aac2e70
ckehrle <christian.kehrle@gmx.de>
2015-08-05 23:05:31 +0200

    plot 2

---
+++ plot2.R
@@ -0,0 +1,29 @@
+#
+library(dplyr)
+
+## This first line will likely take a few seconds. Be patient!
+NEI <- readRDS("./data/summarySCC_PM25.rds")
+SCC <- readRDS("./data/Source_Classification_Code.rds")
+
+agged_data <-   NEI %>%
+                    filter(NEI$fips == "24510") %>%
+                    select (year,Emissions) %>%
+                    group_by(year)  %>%
+                    summarize(total_emissions=sum(Emissions)) %>%
+                    distinct(year,total_emissions)
+
+
+png(filename = "plot2.png",
+    width = 480, height = 480, units = "px", pointsize = 12,
+    bg = "white", res = NA, family = "", restoreConsole = TRUE,
+    type = c("windows", "cairo", "cairo-png"))
+
+plot(agged_data,
+     type="l",
+     col="black",
+     main="Total Emissions over years for Baltimore",
+     ylab="Total Emissions (in kilotons)",
+     xlab='Year',
+     ylim=c(0,8000)
+)
+dev.off()
