6cd691407fca9e1e78eee1c5401f5b2b12862e40
ckehrle <christian.kehrle@gmx.de>
2015-08-23 16:29:37 +0200

    köfte

--- plot6.R
+++ plot6.R
@@ -9,28 +9,23 @@ NEI <- tbl_df(readRDS("./data/summarySCC_PM25.rds"))%>%
     filter(fips == "24510" | fips == "06037")
 
 
-#NEI_SCC <- inner_join(NEI,SCC,by= c("SCC" = "SCC"))
+NEI_SCC <- inner_join(NEI,SCC,by = c("SCC" = "SCC"))
 
-agged_data <- inner_join(NEI,SCC,by= c("SCC" = "SCC"))  %>%
-    select (year,fips,Emissions) #%>%
-    #group_by(year,fips)  %>%
-    #summarize(total_emissions=sum(Emissions)) %>%
-    #distinct(year,fips,total_emissions)%>%
-    #mutate(total_emissions= total_emissions)
+agged_data <- NEI_SCC  %>%
+    select (year,fips,Emissions) %>%
+    group_by(fips,year)  %>%
+    summarize(total_emissions=sum(Emissions)) %>%
+    distinct(year,fips,total_emissions)%>%
+    mutate(total_emissions_delta=abs(total_emissions -lag(total_emissions))) %>%
+    mutate(location=ifelse(fips == "24510","Baltimore City","Los Angeles County"))    
 
+    
 png(filename = "plot6.png",
     width = 480, height = 480, units = "px", pointsize = 12,
     bg = "white", res = NA, family = "", restoreConsole = TRUE,
     type = c("windows", "cairo", "cairo-png"))
 
-print(qplot(year,
-            Emissions,
-            data=agged_data,
-            color= "red",
-            facets= . ~ fips,
-            geom="boxplot",
-            main="Total Emission of PM2.5 by Vehicle sources",
-            ylab='Total Emission of PM2.5 in tons',
-            xlab='Year'))
+print(qplot(total_emissions_delta ,
+            data=agged_data))
 
 dev.off()
